7516949ddb340b55b7daf1ea21d2638d1921a20a
Oleksandr Yanyev <oleksandr.yanyev@durstexpress.eu>
2021-04-12 16:43:32 +0200

    create articles layout

---
+++ Warehouse.Picking/dashboard-app/src/components/Article.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-row>
+    <b-col>
+      <b-img src="./assets/icon-stock.svg"/>
+    </b-col>
+    <b-col>
+      <b-row>{{ name }}</b-row>
+      <b-row>{{ quantity }}</b-row>
+      <b-row>{{ bundles }}</b-row>
+    </b-col>
+    <b-col>
+      <svg class="barcode"
+           jsbarcode-format="ean"
+           :jsbarcode-value="gtin">
+      </svg>
+    </b-col>
+  </b-row>
+</template>
+<script>
+export default {
+  name: "Article",
+  props: ['pic', 'name', 'bundles', 'quantity', 'gtin']
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Warehouse.Picking/dashboard-app/src/components/ArticlesList.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-container id="pickersList">
+    <b-table striped hover :items="articles"></b-table>
+  </b-container>
+</template>
+
+<script src="https://cdn.jsdelivr.net/npm/jsbarcode@3.11.3/dist/barcodes/JsBarcode.ean-upc.min.js">
+export default {
+  name: "ArticlesList",
+  data() {
+    return {
+      articles: []
+    }
+  },
+  mounted() {
+    this.$socket.invoke('GetDeliveryArticles')
+    JsBarcode(".barcode").init();
+  },
+  sockets: {
+    DeliveryArticles(data) {
+      this.articles = []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
