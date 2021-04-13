880e3ef3b5de4b966ac066278ec90ca29684562d
Oleksandr Yanyev <oleksandr.yanyev@durstexpress.eu>
2021-04-13 11:19:51 +0200

    upgrades

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha13")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha14")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
     }
 
