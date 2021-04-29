a888a24d6ea7b864041d5031296a8ac3c5b5e547
Oleksandr Yanyev <oleksandr.yanyev@durstexpress.eu>
2021-04-29 13:34:53 +0200

    sort articles by selected and finished fields

--- android/app/src/main/java/com/eazzyapps/example/android/IntakeActivity.kt
+++ android/app/src/main/java/com/eazzyapps/example/android/IntakeActivity.kt
@@ -1,7 +1,6 @@
 package com.eazzyapps.example.android
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
